caaccc196577cb932cceb8771ec8591e56908e6a
Xangel0s <tu-email@ejemplo.com>
2026-03-14 10:50:25 -0500

    Center compact assay fields

--- src/pages/PlanasForm.tsx
+++ src/pages/PlanasForm.tsx
@@ -426,7 +426,7 @@ export default function PlanasForm() {
                                 <tr>
                                     <td className="border-r border-t border-slate-300 p-1">
                                         <input
-                                            className={denseInputClass}
+                                            className={`${denseInputClass} text-center`}
                                             value={form.muestra}
                                             onChange={(e) => setField('muestra', e.target.value)}
                                             onBlur={() => setField('muestra', normalizeMuestraCode(form.muestra))}
@@ -436,7 +436,7 @@ export default function PlanasForm() {
                                     </td>
                                     <td className="border-r border-t border-slate-300 p-1">
                                         <input
-                                            className={denseInputClass}
+                                            className={`${denseInputClass} text-center`}
                                             value={form.numero_ot}
                                             onChange={(e) => setField('numero_ot', e.target.value)}
                                             onBlur={() => setField('numero_ot', normalizeNumeroOtCode(form.numero_ot))}
@@ -446,7 +446,7 @@ export default function PlanasForm() {
                                     </td>
                                     <td className="border-r border-t border-slate-300 p-1">
                                         <input
-                                            className={denseInputClass}
+                                            className={`${denseInputClass} text-center`}
                                             value={form.fecha_ensayo}
                                             onChange={(e) => setField('fecha_ensayo', e.target.value)}
                                             onBlur={() => setField('fecha_ensayo', normalizeFlexibleDate(form.fecha_ensayo))}
@@ -457,7 +457,7 @@ export default function PlanasForm() {
                                     </td>
                                     <td className="border-t border-slate-300 p-1">
                                         <input
-                                            className={denseInputClass}
+                                            className={`${denseInputClass} text-center`}
                                             value={form.realizado_por}
                                             onChange={(e) => setField('realizado_por', e.target.value)}
                                             autoComplete="off"
